a05ec5f9c06f72c48efe84d32cd7ad3a1114399d
null <null@null>
2024-01-29 01:57:27 +0800

    update quotedb to have bible, better dhammapada, and some fortune related stuff

--- plugin/quotes.py
+++ plugin/quotes.py
@@ -2,7 +2,7 @@ import random
 
 def cmd(author, msg):
     msg = msg.split()
-    commands = ["!dhamma", "!random", "!add", "!help"]
+    commands = ["!dhamma", "!random", "!add", "!8ball", "!fortune", "!bible"]
     output = []
     
     if msg[0] in commands:
@@ -10,6 +10,12 @@ def cmd(author, msg):
             output.append(get_quote("giko-quotes.txt"))
         elif msg[0] == "!dhamma":
             output.append(get_quote("dhammapada.txt"))
+        elif msg[0] == "!bible":
+            output.append(get_quote("bible.txt"))
+        elif msg[0] == "!8ball":
+            output.append(get_quote("8ball.txt"))
+        elif msg[0] == "!fortune":
+            output.append(get_quote("fortunes.txt"))
         elif msg[0] == "!add":
             output.append("Suggest new quotes here: "
                           "https://bbs.gikopoi.com/thread/1705384771/")
