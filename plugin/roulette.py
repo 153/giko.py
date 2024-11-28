241d9a04ba9aa10ef2c51f88cc309046ef631165
null <null@null>
2024-11-29 00:02:47 +0800

    add red/black for roulette

--- plugin/roulette.py
+++ plugin/roulette.py
@@ -36,7 +36,9 @@ def spin(player, bet, amt):
            "odd": [int(i) for i in table[1:] if (int(i) % 2)],
            "low": range(1, 19), "high": range(19, 37),
            "first": range(1, 13), "second": range(13, 25),
-           "third": range(25, 37)}
+           "third": range(25, 37),
+           "red": [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36],
+           "black": [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]}
 
     if bet in win:
            if result in win[bet]:
@@ -66,9 +68,8 @@ def spin(player, bet, amt):
     else:
         output.append("Please enter !spin <bet> <amt>, where "
                       "bet is a single number between 0 and 36 -- payout 35x // "
-                      "even, odd, low (1-18), high (19-36) -- payout 1x // "
-                      "or first (1-12), second (13-24), or third (25-36) "
-                      "-- payout 2x.")
+                      "even, odd, low (1-18), high (19-36), red, black -- payout 1x // "
+                      "or first (1-12), second (13-24), or third (25-36) -- payout 2x.")
     return output
 
 print("Roulette plugin loaded")
